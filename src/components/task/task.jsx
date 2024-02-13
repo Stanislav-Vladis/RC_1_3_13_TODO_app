3e79daca263e4036c4bacbb247d101ff592d72f3
Stanislav Vladislavovich <stanislav.vladis@yandex.ru>
2024-02-13 18:45:28 +0300

    3.2.11 TODO App

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import HookTimer from '../timer/hook-timer.jsx';
 
 export default class Task extends React.Component {
   static propTypes = {
@@ -9,17 +10,19 @@ export default class Task extends React.Component {
     isChecked: PropTypes.bool,
     onEditing: PropTypes.func,
     onCompleted: PropTypes.func,
-    onDestroy: PropTypes.func
+    onDestroy: PropTypes.func,
+    timerControl: PropTypes.object
   };
 
   render() {
-    const { id, description, timeOfCreated, isChecked, onEditing, onCompleted, onDestroy } = this.props;
+    const { id, description, timeOfCreated, isChecked, onEditing, onCompleted, onDestroy, timerControl } = this.props;
 
     return (
       <div className="view">
         <input id={id} className="toggle" type="checkbox" checked={isChecked} onChange={onCompleted} />
         <label htmlFor={id}>
           <span className="description">{description}</span>
+          <HookTimer id={id} timerControl={timerControl} />
           <span className="created">{timeOfCreated}</span>
         </label>
         <button className="icon icon-edit" onClick={onEditing}></button>
